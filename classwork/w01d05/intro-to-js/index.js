4454df1cc95c0b13e938ff1839dbe75a08cafbd8
Melson Fernandes <melson_12@hotmail.com>
2017-08-05 01:00:40 +0100

    Rock paper scissors game fully functioning

--- classwork/w01d05/intro-to-js/index.js
+++ classwork/w01d05/intro-to-js/index.js
@@ -2,9 +2,16 @@ console.log('start finding errors');
 console.log('Error1 Fixed');
 alert('Make some noise!!!!');
 var someVariable = 1;
-someVariable++;
-debugger;
-someVariable++;
-someVariable++;
-
+// someVariable++;
+// debugger;
+// someVariable++;
+// someVariable++;
+namr();
 console.log(someVariable);
+
+function namr() {
+	someVariable+=3;
+}
+
+
+
--- homework/w01d05/app.js
+++ homework/w01d05/app.js
@@ -1,18 +1,165 @@
+
+
 // Edited by Melson.Fernandes
+
+function main() {
 alert("Welcome to Rock, Paper, Scissors Game");
+game1();
 
-var userPick = prompt("Type in your choice | Rock| Paper| Scissors");
-
-var computerPick = Math.random();
-// Math.Random range of 0-1. ()
-if (computerPick < 0.33 ) {
-
-	alert("Rock");
-
-} else if (computerPick <=0.66){
-
-	alert("Paper");
-
-}else {
-	alert("Scissors");
 }
+
+// function userWins(i){
+// 	var i = 0;
+// 	if (i == 5) {alert("user wins");}
+	
+
+// }
+// function compWins(i){
+// 	var i = 0;
+// 	if (i == 5) {alert("computer wins");}
+	
+// }
+var userPoints = 0;
+var ComputerPoints = 0;
+
+
+if (userPoints == 5){
+	alert("User wins with"+userPoints+"ponits")
+
+
+}else if (ComputerPoints == 5){
+	alert("Computer wins with"+ComputerPoints+"ponits")
+	
+}else{
+	game1();
+	
+}
+
+
+
+function game1(){
+var game = null;
+var userPick = prompt("Type in your choice | Rock| Paper| Scissors").toLowerCase();
+var randomCompu = Math.random();
+// Math.Random range of 0-1. ()
+
+
+
+if (userPick === "rock"){
+
+	game = 1;
+}else if (userPick === "paper"){
+	game = 2;
+
+}else if (userPick === "scissors") {
+	game = 3;
+}else{
+	alert("Choose from the three only!");
+}
+
+
+
+var computerPick= null;
+var namepick=null;
+
+if (randomCompu < 0.33 ) {
+	computerPick = 1; //Rock
+	namepick="Rock";
+}else if (randomCompu <=0.66){
+	computerPick = 2; //Paper
+	namepick="Paper";
+}else {
+	computerPick = 3; //Sissors
+	namepick="Sissors";
+}
+
+
+
+
+
+
+// var userPoints = 0;
+// var ComputerPoints = 0;
+
+// Draw
+if (computerPick == 1 && game == 1 ){
+	alert("It is a Draw: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+} else if (computerPick == 2 && game == 2) {
+	alert("It is a Draw: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+} else if (computerPick == 3 && game == 3){
+	alert("It is a Draw: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+}
+
+
+if (computerPick == 1 && game == 2) {
+	userPoints+=1;
+	//countuser(i++);
+	alert("User wins: "+userPick+" vs "+namepick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+	
+	
+}else if (computerPick == 1 && game == 3) {
+	ComputerPoints+=1;
+	//countcomp(i++);
+	alert("Computer wins: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);	
+	
+}
+
+
+
+
+if (computerPick == 2 && game == 1) {
+	ComputerPoints+=1;
+	//countcomp(i++);
+	alert("Computer wins: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+	
+	
+}else if (computerPick == 2 && game == 3) {
+	userPoints+=1;
+	//countuser(i++);
+	alert("User wins: "+userPick+" vs "+namepick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+	
+	
+}else if (computerPick == 3 && game == 2) {
+	ComputerPoints+=1;
+	//countcomp(i++);
+	alert("Computer wins: "+namepick+" vs "+userPick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+	
+
+}else if (computerPick == 3 && game == 1) {
+	userPoints+=1;
+	//countuser(i++);
+	alert("User wins: "+userPick+" vs "+namepick);
+	alert("User: "+ userPoints+" | | "+"Computer Points: "+ComputerPoints);
+	
+	
+}
+
+
+
+game1();
+
+
+}
+ main();
+
+
+
+
+
+
+
