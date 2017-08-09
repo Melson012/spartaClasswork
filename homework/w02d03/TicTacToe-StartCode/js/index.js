fdabc2de53442d12d1e93790a5dd59cfe20a613b
Melson Fernandes <melson_12@hotmail.com>
2017-08-09 22:19:33 +0100

    tic tac toe- no win added

--- homework/w02d03/TicTacToe-StartCode/js/index.js
+++ homework/w02d03/TicTacToe-StartCode/js/index.js
@@ -1,22 +1,53 @@
 $(function(event){
 	
-var player1Score = null;
-var player2Score = null;
-var possibleWins = [];
+var player1Score = "X";
+var player2Score = "O";
+var possibleWins = [[0,1,2]];
+var player1Combination = [0,1,2];
+var player2Combination = [];
+var compareArray = [];
 var boxes = $("td");
+var X = true;
+var O = false;
+var i = 0;
+var r = 0;
 $(boxes).each(function(index,box){
 	onClick(box);
 })
 function onClick(box1){
-		//console.log($(box).attr("data-num"));
 		 $(box1).on('click', function(){
-		 	console.log($(box1).attr("data-num"))
+			if (i%2==0){
+		 		$(box1).html(player1Score);
+		 		$(box1).addClass(player1Score);
+		 		i+=1;
+		 		player1Combination.push(parseInt($(box1).attr("data-num")))
+		 		console.log(player1Combination);
+		 	}else {
+		 		$(box1).html(player2Score);
+		 		$(box1).addClass(player2Score);
+		 		i+=1;
+		 		player2Combination.push(parseInt($(box1).attr("data-num")))
+		 		console.log(player2Combination);
+			}
+
+			// if (player1Combination.lenght <=3 || player2Combination.lenght <=3){
+			// 	compareArray.push(player2Combination);
+			// 	console.log(compareArray);
+			// }
 		 })
 }
+console.log(player1Combination===possibleWins);
+//console.log(player1Combination.[0,1,2]);
+function logic(){
 
-
-
-
+	possibleWins.each(function(i,val) {
+	    player1Combination.each(function(j,val_j) {
+	    	
+	    	
+	    });
+	});
+	
+}
 
 });
 
