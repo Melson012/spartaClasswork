7d5b8ff7b697927638eeec36886ed0fba342c2ec
Melson Fernandes <melson_12@hotmail.com>
2017-08-29 21:48:44 +0100

    fully working youtube with bootstrap

--- homework/w05d02/youtube/db/seeds.rb
+++ homework/w05d02/youtube/db/seeds.rb
@@ -2,7 +2,13 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 Video.destroy_all
 Video.create([
-	{title: 'Mask Off', url: 'https://www.youtube.com/embed/xvZqHgFz51I', description: 'solid gold.'},
-	{title: 'Unforgetable', url: 'https://www.youtube.com/embed/CTFtOOh47oo', description: 'solid gold.'}
+	{title: 'Mask Off', url: 'https://www.youtube.com/embed/xvZqHgFz51I', description: 'Published on May 5, 2017
+"FUTURE" 
+'},
+	{title: 'Unforgetable', url: 'https://www.youtube.com/embed/CTFtOOh47oo', description: 'Published on Apr 13, 2017
+"Unforgettable" 
+'},
+	{title: 'White Iverson', url: 'https://www.youtube.com/embed/CTFtOOh47oo', description: 'Published on Jul 19, 2015
+Post Malone'}
 	
 ])
