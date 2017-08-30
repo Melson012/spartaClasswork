63b1e3a043da0ff35254a5599b99c83ff4dceba3
Melson Fernandes <melson_12@hotmail.com>
2017-08-30 21:05:31 +0100

    author name displays on books

--- homework/relationship/db/seeds.rb
+++ homework/relationship/db/seeds.rb
@@ -2,9 +2,13 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 	Book.create([
-		{title: 'Harry Potter 1', author_id: 1}
+		{title: 'Harry Potter and the Philosophers Stone', author_id: 1},
+		{title: 'Harry Potter and the Prisoner of Azkaban', author_id: 1},
+		{title:'Charlie and the Chocolate Factory', author_id: 2},
+		{title:'The BFG', author_id: 2}
 	])
 
 	Author.create([
-		{name:'J. K. Rowling'}
+		{name:'J. K. Rowling'},
+		{name:'Roald Dahl'}
 	])
